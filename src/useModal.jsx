a84b1e07fa7cdd111d26844e420a3c85de43b587
ChuckBDT <charlesboudrant@protonmail.com>
2023-04-29 09:55:51 +0200

    TailwindCSS => Native CSS to reduce dependencies

--- src/useModal.jsx
+++ src/useModal.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import "./assets/useModal.css";
 
 /**
  * Custom React hook for creating a modal
@@ -34,28 +35,26 @@ function useModal() {
     if (open) {
       document.body.style.overflow = "hidden";
       return (
-        <div className='useModal-bg fixed left-0 top-0 z-10 h-screen w-screen bg-primary/25 overflow-hidden flex justify-center items-center'>
-          <div
-            ref={modalRef}
-            className='useModal-frame relative bg-tertiary p-8 m-4 rounded-2xl shadow-lg'
-          >
-            <div
-              className='absolute top-3 right-3 cursor-pointer bg-secondary hover:bg-primary/10 rounded-md p-1'
-              onClick={() => triggerModal()}
-            >
+        <div className='useModal'>
+          <div ref={modalRef} className='useModal-frame'>
+            <div className='useModal-closeBtn' onClick={() => triggerModal()}>
               <svg
-                stroke='currentColor'
-                className='fill-primary'
                 strokeWidth='0'
-                viewBox='0 0 1024 1024'
-                height='1.1em'
-                width='1.1em'
+                viewBox='0 0 512 512'
+                height='1.5em'
+                width='1.5em'
                 xmlns='http://www.w3.org/2000/svg'
               >
-                <path d='M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z'></path>
+                <path
+                  fill='none'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='32'
+                  d='M368 368L144 144m224 0L144 368'
+                ></path>
               </svg>
             </div>
-            <div className='useModal-content py-6'>{content}</div>
+            <div className='useModal-content'>{content}</div>
           </div>
         </div>
       );
